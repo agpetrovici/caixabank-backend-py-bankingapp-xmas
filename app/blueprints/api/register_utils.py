2c1fcfe1e27c4f7dbcfddf83097497d4052e249c
apetrovici <alejandropetrovici@gmail.com>
2024-12-29 08:08:10 +0100

    fix: validate_registration_data

--- app/blueprints/api/register_utils.py
+++ app/blueprints/api/register_utils.py
@@ -5,16 +5,16 @@ from pyisemail import is_email
 from app.models import User
 
 
-def validate_registration_data(
-    data: dict,
-) -> tuple[dict | None, tuple[dict, int] | None]:
+def validate_registration_data(data: dict) -> tuple[dict, bool, int]:
+    status = False
+    code = 400
     # Check if all required fields are present
     if not all(key in data for key in ["email", "password", "name"]):
-        return None, ({"error": "All fields are required."}, 400)
+        return {"error": "All fields are required."}, status, code
 
     # Check for null/empty fields
     if not all(data.values()):
-        return None, ({"error": "No empty fields allowed."}, 400)
+        return {"error": "No empty fields allowed."}, status, code
 
     email = data["email"]
     password = data["password"]
@@ -22,13 +22,15 @@ def validate_registration_data(
 
     # Validate email format
     if not is_email(email):
-        return None, ({"error": f"Invalid email: {email}"}, 400)
+        return {"error": f"Invalid email: {email}"}, status, code
 
     # Clean email by converting to lowercase and stripping whitespace
     email = email.lower().strip()
 
     # Check if email already exists
     if User.query.filter_by(email=email).first():
-        return None, ({"error": "Email already exists."}, 400)
+        return {"error": "Email already exists."}, status, code
 
-    return {"email": email, "password": password, "name": name}, None
+    status = True
+    code = 200
+    return {"email": email, "password": password, "name": name}, status, code
