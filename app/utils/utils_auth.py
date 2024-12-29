9d26ef40f0502f6c91c357700cbddba99eb903a0
apetrovici <alejandropetrovici@gmail.com>
2024-12-29 13:18:59 +0100

    disable!: data null/empty check

--- app/utils/utils_auth.py
+++ app/utils/utils_auth.py
@@ -61,12 +61,12 @@ def validate_registration_data(data: dict) -> tuple[dict, bool, int]:
     status = False
     code = 400
     # Check if all required fields are present
-    if not all(key in data for key in ["email", "password", "name"]):
-        return {"msg": "All fields are required."}, status, code
+    # if not all(key in data for key in ["email", "password", "name"]):
+    #     return {"msg": "All fields are required."}, status, code
 
-    # Check for null/empty fields
-    if not all(data.values()):
-        return {"msg": "No empty fields allowed."}, status, code
+    # # Check for null/empty fields
+    # if not all(data.values()):
+    #     return {"msg": "No empty fields allowed."}, status, code
 
     email = data["email"]
     password = data["password"]
