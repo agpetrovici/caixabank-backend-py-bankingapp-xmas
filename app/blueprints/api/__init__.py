dfa16f7f79d3234d78bcd233cc0f7c9eb75d8e03
apetrovici <alejandropetrovici@gmail.com>
2024-12-30 16:48:29 +0100

    Revert "chore: return fraud: int"
    This reverts commit cf4cf2529b3b44947fe1f3a9cee0d2e1acf7bf10.

--- app/blueprints/api/__init__.py
+++ app/blueprints/api/__init__.py
@@ -698,7 +698,7 @@ def add_transaction():
             amount=amount,
             category=category,
             timestamp=timestamp,
-            fraud=int(is_fraud),
+            fraud=is_fraud,
         )
 
         # Update user balance
